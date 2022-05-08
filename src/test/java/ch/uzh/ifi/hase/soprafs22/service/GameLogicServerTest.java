49f269fde8528145a581a0f4cfadea1856e24380
shmnrr <91474287+shmnrr@users.noreply.github.com>
2022-05-08 22:21:43 +0200

    Test cleanup

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/GameLogicServerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/GameLogicServerTest.java
@@ -26,14 +26,16 @@ public class GameLogicServerTest {
     private Ball green14 = new Ball(Color.GREEN, 14);
     private Ball green16 = new Ball(Color.GREEN, 16);
     private Ball green80 = new Ball(Color.GREEN, 80);
-    private Ball red84 = new Ball(Color.RED, 84);
-    private Ball yellow88 = new Ball(Color.YELLOW, 88);
-    private Ball blue95 = new Ball(Color.BLUE, 95);
-
     private Ball green63 = new Ball(Color.GREEN, 63);
+
+    private Ball red84 = new Ball(Color.RED, 84);
     private Ball red15 = new Ball(Color.RED, 15);
+
+    private Ball yellow88 = new Ball(Color.YELLOW, 88);
     private Ball yellow31 = new Ball(Color.YELLOW, 31);
+
     private Ball blue47 = new Ball(Color.BLUE, 47);
+    private Ball blue95 = new Ball(Color.BLUE, 95);
 
 
     @BeforeEach
@@ -97,56 +99,38 @@ public class GameLogicServerTest {
 
     @Test
     public void ballChosen_validPossibleDestinations() {
-
-    Set<Integer> possibleDestinations1o11 = new HashSet<>(Set.of(1, 11));
-    Set<Integer> possibleDestinationsn4o4 = new HashSet<>(Set.of(-4, 4));
-    Set<Integer> possibleDestinations1o11o100 = new HashSet<>(Set.of(1, 11, 100));
-    Set<Integer> possibleDestinations5 = new HashSet<>(Set.of(5));
-
-
-    Set<Integer> testPossibleDestinations1 = new HashSet<>(Set.of(1, 11));
-    Set<Integer> testPossibleDestinations2 = new HashSet<>(Set.of(0, 10));
-    Set<Integer> testPossibleDestinationsGREEN = new HashSet<>(Set.of(0));
-    Set<Integer> testPossibleDestinationsRED = new HashSet<>(Set.of(16));
-    Set<Integer> testPossibleDestinationsYELLOW = new HashSet<>(Set.of(32));
-    Set<Integer> testPossibleDestinationsBLUE = new HashSet<>(Set.of(48));
-
-    Set<Integer> testPossibleDestinations3 = new HashSet<>(Set.of(4, 60));
-
     // GOING INTO BASE WITH DIFFERENT COLORS
-        Set<Integer> testPossibleDestinationsGREEN1 = new HashSet<>(Set.of(4,67));
-        Set<Integer> testPossibleDestinationsRED1 = new HashSet<>(Set.of(20,71));
-        Set<Integer> testPossibleDestinationsYELLOW1 = new HashSet<>(Set.of(36,75));
-        Set<Integer> testPossibleDestinationsBLUE1 = new HashSet<>(Set.of(79,52));
+        Set<Integer> possibleDestinationsGREEN1 = gameLogicService.getPossibleDestinations(Set.of(5), green63);
+        Set<Integer> possibleDestinationsGREENinBase = gameLogicService.getPossibleDestinations(Set.of(1,2,3), green64);
 
-        Set<Integer> possibleDestinationsGREEN1 = gameLogicService.getPossibleDestinations(possibleDestinations5, green63);
-        Set<Integer> possibleDestinationsRED1 = gameLogicService.getPossibleDestinations(possibleDestinations5, red15);
-        Set<Integer> possibleDestinationsYELLOW1 = gameLogicService.getPossibleDestinations(possibleDestinations5, yellow31);
-        Set<Integer> possibleDestinationsBLUE1 = gameLogicService.getPossibleDestinations(possibleDestinations5, blue47);
+        Set<Integer> possibleDestinationsRED1 = gameLogicService.getPossibleDestinations(Set.of(5), red15);
+        Set<Integer> possibleDestinationsYELLOW1 = gameLogicService.getPossibleDestinations(Set.of(5), yellow31);
+        Set<Integer> possibleDestinationsBLUE1 = gameLogicService.getPossibleDestinations(Set.of(5), blue47);
 
-        assertEquals(testPossibleDestinationsGREEN1, possibleDestinationsGREEN1);
-        assertEquals(testPossibleDestinationsRED1, possibleDestinationsRED1);
-        assertEquals(testPossibleDestinationsYELLOW1, possibleDestinationsYELLOW1);
-        assertEquals(testPossibleDestinationsBLUE1, possibleDestinationsBLUE1);
+        assertEquals(Set.of(4,67), possibleDestinationsGREEN1);
+        assertEquals(Set.of(20,71), possibleDestinationsRED1);
+        assertEquals(Set.of(36,75), possibleDestinationsYELLOW1);
+        assertEquals(Set.of(79,52), possibleDestinationsBLUE1);
+        assertEquals(Set.of(65,66,67), possibleDestinationsGREENinBase);
 
-    Set<Integer> possibleDestinations1 = gameLogicService.getPossibleDestinations(possibleDestinations1o11, green0);
-    Set<Integer> possibleDestinations2 = gameLogicService.getPossibleDestinations(possibleDestinations1o11, green63);
+    Set<Integer> possibleDestinations1 = gameLogicService.getPossibleDestinations(Set.of(1, 11), green0);
+    Set<Integer> possibleDestinations2 = gameLogicService.getPossibleDestinations(Set.of(1, 11), green63);
 
-    Set<Integer> possibleDestinationsGREEN = gameLogicService.getPossibleDestinations(possibleDestinations1o11o100, green80);
-    Set<Integer> possibleDestinationsRED = gameLogicService.getPossibleDestinations(possibleDestinations1o11o100, red84);
-    Set<Integer> possibleDestinationsYELLOW = gameLogicService.getPossibleDestinations(possibleDestinations1o11o100, yellow88);
-    Set<Integer> possibleDestinationsBLUE = gameLogicService.getPossibleDestinations(possibleDestinations1o11o100, blue95);
+    Set<Integer> possibleDestinationsGREEN = gameLogicService.getPossibleDestinations(Set.of(1, 11, 100), green80);
+    Set<Integer> possibleDestinationsRED = gameLogicService.getPossibleDestinations(Set.of(1, 11, 100), red84);
+    Set<Integer> possibleDestinationsYELLOW = gameLogicService.getPossibleDestinations(Set.of(1, 11, 100), yellow88);
+    Set<Integer> possibleDestinationsBLUE = gameLogicService.getPossibleDestinations(Set.of(1, 11, 100), blue95);
 
-    Set<Integer> possibleDestinations3 = gameLogicService.getPossibleDestinations(possibleDestinationsn4o4, green0);
+    Set<Integer> possibleDestinations3 = gameLogicService.getPossibleDestinations(Set.of(-4, 4), green0);
 
-    assertEquals(testPossibleDestinations1, possibleDestinations1);
-    assertEquals(testPossibleDestinations2, possibleDestinations2);
-    assertEquals(testPossibleDestinations3, possibleDestinations3);
+    assertEquals(Set.of(1, 11), possibleDestinations1);
+    assertEquals(Set.of(0, 10), possibleDestinations2);
+    assertEquals(Set.of(4, 60), possibleDestinations3);
 
-    assertEquals(testPossibleDestinationsGREEN, possibleDestinationsGREEN);
-    assertEquals(testPossibleDestinationsRED, possibleDestinationsRED);
-    assertEquals(testPossibleDestinationsYELLOW, possibleDestinationsYELLOW);
-    assertEquals(testPossibleDestinationsBLUE, possibleDestinationsBLUE);
+    assertEquals(Set.of(0), possibleDestinationsGREEN);
+    assertEquals(Set.of(16), possibleDestinationsRED);
+    assertEquals(Set.of(32), possibleDestinationsYELLOW);
+    assertEquals(Set.of(48), possibleDestinationsBLUE);
     }
 
     @Test
